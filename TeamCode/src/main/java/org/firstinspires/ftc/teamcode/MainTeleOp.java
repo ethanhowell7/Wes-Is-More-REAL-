335e634326b65293a46693dad6723edba2ce9c01
galta <galtaffer07@gmail.com>
2023-03-24 19:26:22 -0400

    added more stuff but now tired so probably sleepy time?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -7,11 +7,23 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@TeleOp (name = "TestTeleOp", group =  "Please work")
 public class MainTeleOp extends LinearOpMode {
+
+
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         waitForStart();
+
+        while(opModeIsActive()){
+
+            idle();
+        }
     }
 }
